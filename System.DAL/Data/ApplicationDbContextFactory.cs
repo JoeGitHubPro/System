ac2224c741c668e8bd3da0debbbc21b6ce6451ab
Youssef Mohamed Ali <95888292+JoeGitHubPro@users.noreply.github.com>
2024-07-26 11:59:58 +0300

    Add Factory Class

---
+++ System.DAL/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace System.DAL.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-T4OMHBE\\SQLEXPRESS;Initial Catalog=N-TierDB; Integrated Security = SSPI ; TrustServerCertificate = True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
