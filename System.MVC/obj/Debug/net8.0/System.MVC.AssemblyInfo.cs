c9b41b4f210b55edcca008f3a625ce19b59e7058
Youssef Mohamed Ali <95888292+JoeGitHubPro@users.noreply.github.com>
2024-08-02 18:56:14 +0300

    Roles Management

--- System.MVC/obj/Debug/net8.0/System.MVC.AssemblyInfo.cs
+++ System.MVC/obj/Debug/net8.0/System.MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("System.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce57dde45de8aec293ce5f19327986190f4110a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b58c1ebd844159b0c5d5e8e03864ed68d07862")]
 [assembly: System.Reflection.AssemblyProductAttribute("System.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("System.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
